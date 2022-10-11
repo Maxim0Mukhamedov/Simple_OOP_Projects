1f088059d1c5e210135a75383d26272372fecc91
Maxim0Mukhamedov <113615768+Maxim0Mukhamedov@users.noreply.github.com>
2022-10-11 15:11:41 +0300

    Create 0951a.cpp

---
+++ 0951a.cpp
@@ -0,0 +1,17 @@
+    #include <iostream>
+     
+    int main() {
+        int n,k;
+        std::cin >> n >> k;
+        int a_i;
+        int m = -1;
+        for (int i = 0; i < n; i += 1) {
+            std::cin >> a_i;
+            if (a_i <= k && k % a_i == 0) {
+                if (a_i > m) {
+                    m = a_i;
+                }
+            }
+        }
+        std::cout << k / m;
+    }
