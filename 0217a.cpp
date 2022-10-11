0100c32812d92e7df980a2715e44d9d89d3975f0
Maxim0Mukhamedov <113615768+Maxim0Mukhamedov@users.noreply.github.com>
2022-10-11 15:15:27 +0300

    Create 0217a.cpp

---
+++ 0217a.cpp
@@ -0,0 +1,23 @@
+    #include <iostream>
+     
+    int main()
+    {
+        int y = 0;
+        std::cin >> y;
+        for (y += 1; y <= 9999; y += 1 ) {
+            int s1 = y / 1000;
+            int s2 = (y - (1000 * s1)) / 100;
+            int s3 = (y - (1000 * s1) - 100 * s2) / 10;
+            int s4 = y % 10;
+            bool b = s1 != s2;
+            bool b1 = s1 != s3;
+            bool b2 = s1 != s4;
+            bool b3 = s2 != s3;
+            bool b4 = s2 != s4;
+            bool b5 = s3 != s4;
+            if (b1 && b2 && b3 && b4 && b5 && b) {
+                std::cout << y;
+                break;
+            }
+        }
+    }
