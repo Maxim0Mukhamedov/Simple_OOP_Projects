b0d3bc2acf97beada44b59fcee6c62d402ffe429
Maxim0Mukhamedov <113615768+Maxim0Mukhamedov@users.noreply.github.com>
2022-10-11 15:25:39 +0300

    Update hw-05-02.cpp

--- hw-05-02.cpp
+++ hw-05-02.cpp
@@ -17,12 +17,14 @@ int main() {
     std::cout.flags(std::ios::left);
     std::cout << '|' << std::setw(10) << 'x' << '|' << std::setw(10) << "s(x)" << '|' << std::setw(10) << "f(x)" << '|'
               << '\n';
-    for (i = 0, a; a <= b && (p == 0 || std::abs(p) > epsilon); a += delta, i += 1) {
+    for (i = 0, a; a <= b; a += delta, i += 1) {
         x = a;
+        s = 0;
+        p = 0;
         if (std::abs(x) < delta) {
             x = 0;
         }
-        for (i = 0; std::abs(p) > epsilon && p != 0; i += 1) {
+        for (i = 0; std::abs(p) > epsilon || p == 0 && x != 0; i += 1) {
             p = std::pow(x, i) * std::sin(i * M_PI / 4);
             s += p;
         }
@@ -30,3 +32,4 @@ int main() {
         std::cout << '|' << std::setw(10) << x << '|' << std::setw(10) << s << '|' << std::setw(10) << f << '|' << '\n';
     }
 }
+
