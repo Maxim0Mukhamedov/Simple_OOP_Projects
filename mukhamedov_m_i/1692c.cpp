971519914f01faf63fedb8b253b5d10ef7af0471
Maxim0Mukhamedov <xapestatf@gmail.com>
2022-10-18 16:03:43 +0300

    _

--- mukhamedov_m_i/1692c.cpp
+++ mukhamedov_m_i/1692c.cpp
@@ -1,26 +1,32 @@
-//#include <iostream>
-//#include <string>
-//#include <vector>
-//
-//int main() {
-//    int t;
-//    std::cin >> t;
-//    for (int i = 0; i < t; i += 1) {
-//        std::string s;
-//        std::vector<int> a;
-//        int cntStar = 0;
-//        for (int j = 0; j < 8; ++j) {
-//            std::cin >> s;
-//            for (int k = 0; k < 8; ++k) {
-//                if (s[j] == '*') {
-//                    cntStar += 1;
-//                }
-//            }
-//            if (cntStar == 1) {
-//                for (int k = 0, k < 8; ++k) {
-//                    if
-//                }
-//            }
-//        }
-//    }
-//}
+#include <iostream>
+#include <string>
+#include <vector>
+
+int main() {
+    int t;
+    std::cin >> t;
+    for (int i = 0; i < t; i += 1) {
+        std::string s;
+        std::vector<int> a;
+        int cntStar = 0;
+        int ans = 0;
+        for (int j = 0; j < 8; ++j) {
+            std::cin >> s;
+            cntStar = 0;
+            for (int k = 0; k < 8; ++k) {
+                if (s[j] == '#') {
+                    cntStar += 1;
+                }
+            }
+            ans = 0;
+            if (cntStar == 1) {
+                for (int k = 0; k < 8; ++k) {
+                    if (s[j] == '#') {
+                        ans = (j+1)*10 + (k + 1);
+                    }
+                }
+            }
+        }
+        std::cout << ans/10 << ' ' << ans % 10 << '\n';
+    }
+}
