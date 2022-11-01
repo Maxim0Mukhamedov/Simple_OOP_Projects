80e804540f6dc59a894c52d7272bbc7ffc995106
Maxim0Mukhamedov <113615768+Maxim0Mukhamedov@users.noreply.github.com>
2022-11-01 13:22:34 +0300

    Update 1501a.cpp

--- mukhamedov_m_i/1501a.cpp
+++ mukhamedov_m_i/1501a.cpp
@@ -1,31 +1,33 @@
 #include <iostream>
-#include <iomanip>
-#include <cmath>
-#include <algorithm>
-#define _USE_MATH_DEFINES
-
-// Доделать дома!!!!!!! Смассивом.
-
+#include <vector>
+ 
 int main() {
-    int t;
-    std::cin >> t;
-    for (int i = 0; i < t; i += 1) {
-        int n;
-        std::cin >> n;
-        int timer = 0;
-        int b_iLast = 0;
-        int a_i,b_i;
-        for (int j = 0; j < n; j += 1) {
-            std::cin >> a_i >> b_i;
-            timer += (-b_iLast + a_i) + (b_i + 1 - a_i)/2;
-            b_iLast = b_i;
-        } 
-        timer -=  (b_i + 1 - a_i)/2;
-        int tm_i;
-        for (int j = 0; j < n; j += 1) {
-            std::cin >> tm_i;
-            timer += tm_i;
-        }
-        std::cout << timer << '\n';
-    }
+	int t;
+	std::cin >> t;
+	for (int k = 0; k < t; ++k) {
+		int n;
+		std::cin >> n;
+		int ai = 0, bi = 0;
+		int time = 0;
+		std::vector <int> a (101);
+		std::vector <int> b (101);
+		std::vector <int> t (101);
+		for (int i = 1; i <= n; ++i) {
+			std::cin >> a[i] >> b[i];
+		}
+		for (int i = 1; i <= n; ++i) {
+			std::cin >> t[i];
+		}
+		for (int i = 0; i < n; ++i) {
+			if (b[i] > time + (b[i] - a[i] + 1) / 2) {
+				time = b[i];
+			}
+			else {
+				time = time + (b[i] - a[i] + 1) / 2;
+			}
+			time += a[i + 1] - b[i] + t[i + 1];
+		}
+		std::cout << time << std::endl;
+	}
+	
 } 
