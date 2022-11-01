6c9aeed6431062a3177a82602bee0e3f0cc405e5
Maxim0Mukhamedov <113615768+Maxim0Mukhamedov@users.noreply.github.com>
2022-11-01 13:36:45 +0300

    Create 1699b.cpp

---
+++ mukhamedov_m_i/1699b.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+int main()
+{
+    int t;
+    std::cin>>t;
+    for (int k = 0; k < t; k += 1){
+        int n,m;
+        std::cin >> n >> m;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                std::cout<<((i%4<=1)!=(j%4<=1))<< ' ';
+            }
+            std::cout << '\n';
+        }
+    }
+    return 0;
+}
