b2d29922a05d504e690f6303ae1f0d68770882e8
Maxim0Mukhamedov <113615768+Maxim0Mukhamedov@users.noreply.github.com>
2023-03-04 13:17:34 +0300

    Delete arrayd directory

--- arrayd/main.cpp
+++
@@ -1,8 +0,0 @@
-#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
-#include "arrayd/arrayd.h"
-#include "doctest/doctest.h"
-TEST_CASE("testing the DynamicArrays functions") {
-    CHECK_NOTHROW(ArrayD{1, 2});
-    CHECK_NOTHROW(ArrayD{1, 2}.size());
-    CHECK_NOTHROW(ArrayD{1, 2}.push_back(3));
-}
