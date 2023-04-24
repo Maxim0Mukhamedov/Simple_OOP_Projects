b82ddee841f2dff094b29d14814c7f49030bcbb1
Maxim0Mukhamedov <m2204642@edu.misis.ru>
2023-04-24 09:36:55 +0300

    Changes to be committed: 	modified:   prj.lab/arrayt/CMakeLists.txt 	modified:   prj.lab/arrayt/include/arrayt/arrayt.hpp 	modified:   prj.test/arrayt_test.cpp

--- mukhamedov_m_i/prj.lab/arrayt/include/arrayt/arrayt.hpp
+++ mukhamedov_m_i/prj.lab/arrayt/include/arrayt/arrayt.hpp
@@ -2,143 +2,139 @@
 #define ARRAYT_HPP
 
 
-#include <initializer_list>
 #include <cstddef>
-#include "stdexcept"
 #include <iostream>
 #include <algorithm>
-//
-template<typename T>
-class ArrayT
-{
+#include <stdexcept>
+#include <cstdint>
+
+template <typename T>
+class ArrayT {
 public:
     // Конструкторы
-    ArrayT(int s = 0);
+    explicit ArrayT(const int s = 0);
     ArrayT(const ArrayT<T>& other);
-    ArrayT(std::initializer_list<T> list);
     ArrayT& operator=(const ArrayT<T>& other);
-    //Деструктор
+    // Деструктор
     ~ArrayT() = default;
-    //Оператор итерирования
-    const T& operator[](const int index) const;
-    T& operator[](const int index);
-    //Взятие размера
-    ptrdiff_t ssize() const;
-    //Изменение размера
+    // Оператор итерирования
+    const T& operator[](const int& index) const;
+    T& operator[](const int& index);
+    // Взятие размера
+    [[nodiscard]] int32_t ssize() const;
+    // Изменение размера
     void resize(const int& size);
-    //insert remove
     void insert(const int& i, const T& elem);
     void remove(const int& i);
-    //Ввод/вывод
+    // Ввод/вывод
     std::istream& ReadFrom(std::istream& istrm);
-    std::ostream& WriteTo(std::ostream& ostrm);
+    std::ostream& WriteTo(std::ostream& ostrm); //const
+
 private:
-    T* data = nullptr;
+    T *data = nullptr;
     std::ptrdiff_t ssize_ = 0;
     std::ptrdiff_t capacity_ = 0;
 };
-//
-template<typename T>
+template <typename T>
 std::ostream& operator<<(std::ostream& ostrm, ArrayT<T>& array);
-template<typename T>
+template <typename T>
 std::istream& operator>>(std::istream& istrm, ArrayT<T>& array);
 
-template<typename T>
-ArrayT<T>::ArrayT(int s) : ssize_(s) { data = new T[ssize_]; }
+template <typename T>
+ArrayT<T>::ArrayT(const int s) : ssize_(s) {
+    if (s < 0) { throw std::invalid_argument("invalid size");}
+    capacity_ = ssize_ * 2;
+    data = new T[capacity_]{};
+}
 
-template<typename T>
-ArrayT<T>::ArrayT(const ArrayT<T> &other) : ssize_(other.ssize_)
-{
+template <typename T>
+ArrayT<T>::ArrayT(const ArrayT<T> &other) : ssize_(other.ssize_) {
     capacity_ = ssize_*2;
     data = new T[capacity_];
-    for(int i = 0; i < ssize_; ++i)
-        data[i] = other.data[i];
 }
 
-template<typename T>
-ArrayT<T>::ArrayT(std::initializer_list<T> list) : ssize_(list.size())
-{
-    data = new T[ssize_];
-    int i = 0;
-    for (auto& item : list)
-        data[i++] = item;
-}
-template<typename T>
+template <typename T>
 ArrayT<T>& ArrayT<T>::operator=(const ArrayT<T> &other) {
-    delete[] data;
-    ssize_ = other.ssize();
-    data = new T[ssize_];
-    for(int i = 0; i < ssize_; ++i)
-        data[i] = other.data[i];
-}
-template<typename T>
-T &ArrayT<T>::operator[](int index) {
-    if (index <= 0 || index >= ssize_) {throw std::out_of_range("invalid index");}
-    return data[index];
-}
-
-template<typename T>
-const T& ArrayT<T>::operator[](const int index) const {
-    if (index <= 0 || index >= ssize_) {throw std::out_of_range("invalid index");}
-    return data[index];
-}
-
-template<typename T>
-ptrdiff_t ArrayT<T>::ssize() const { return ssize_; }
-
-template<typename T>
-void ArrayT<T>::resize(const int& new_size){
-    if (new_size <= 0) { throw std::out_of_range("invalid size");}
-    T* old = data;
-    ssize_ = new_size;
-    data = new T[new_size];
-    int min_size = size_t(old) < new_size ? size_t(old) : ssize_;
-    for (int i = 0; i < min_size; ++i) {
-        data[i] = old[i];
+    if (data != other.data) {
+        delete[] data;
+        ssize_ = other.ssize();
+        capacity_ = ssize_ * 2;
+        data = new T[capacity_];
+        for (int i = 0; i < ssize_; ++i)
+            data[i] = other.data[i];
     }
-    delete[] old;
+    return *this;
 }
 
-template<typename T>
+template <typename T>
+T &ArrayT<T>::operator[](const int& index) {
+    if (index < 0 || index >= ssize_) {
+        throw std::out_of_range("invalid index");
+    }
+    return data[index];
+}
+
+template <typename T>
+const T& ArrayT<T>::operator[](const int& index) const {
+    if (index < 0 || index >= ssize_) {
+        throw std::out_of_range("invalid index");
+    }
+    return data[index];
+}
+
+template <typename T>
+int32_t ArrayT<T>::ssize() const { return ssize_; }
+
+template <typename T>
+void ArrayT<T>::resize(const int& new_size) {
+    if (new_size <= 0) { throw std::invalid_argument("invalid size");}
+    if (capacity_ < new_size) {
+        T* old = data;
+        capacity_ = new_size * 2;
+        data = new T[capacity_];
+        for (int i = 0; i < new_size; i++) {
+            data[i] = old[i];
+        }
+        delete[] old;
+    }
+    ssize_ = new_size;
+}
+
+template <typename T>
 void ArrayT<T>::insert(const int& i, const T& elem) {
-    if (i <= 0 || i > ssize_) {throw std::out_of_range("invalid index");}
-    ssize_ += 1;
-    T* old = data;
-    data = new T[ssize_];
-    for (int j = 0; j < i; ++j) {
-        data[j] = old[j];
+    if (i < 0 || i > ssize_) {throw std::out_of_range("invalid index");}
+    (*this).resize(ssize_ + 1);
+    for (int j = ssize_ - 1; j > i; --j) {
+        data[j] = data[j - 1];
     }
     data[i] = elem;
-    for (int j = i + 1; j < ssize_; ++j) {
-        data[j] = old[j - 1];
-    }
-    delete[] old;
 }
 
-template<typename T>
+template <typename T>
 void ArrayT<T>::remove(const int& i) {
+    if (i < 0 || i >= ssize_) {throw std::out_of_range("invalid index");}
     std::rotate(data + i, data + i + 1, data + ssize_);
     (*this).resize(ssize_ - 1);
 }
 
-template<typename T>
+template <typename T>
 std::istream &ArrayT<T>::ReadFrom(std::istream &istrm) {
     if (ssize_ > 0) {
         int i = 0;
         T xi = 0;
-        while (!istrm.eof() && istrm.good() && i < ssize_){
+        while (!istrm.eof() && istrm.good() && i < ssize_) {
             istrm >> xi;
             data[i] = xi;
             i++;
         }
     } else {
-        throw std::exception();
+        istrm.setstate(std::ios_base::failbit);
     }
     return istrm;
 }
 
-template<typename T>
-std::ostream& ArrayT<T>::WriteTo(std::ostream &ostrm) {
+template <typename T>
+std::ostream &ArrayT<T>::WriteTo(std::ostream &ostrm) {
     if (ssize_ > 0) {
         int i = 0;
         while (ostrm.good() && i < ssize_) {
@@ -149,14 +145,13 @@ std::ostream& ArrayT<T>::WriteTo(std::ostream &ostrm) {
     return ostrm;
 }
 
-template<typename T>
+template <typename T>
 std::ostream &operator<<(std::ostream &ostrm, ArrayT<T> &array) {
     return array.WriteTo(ostrm);
 }
 
-template<typename T>
+template <typename T>
 std::istream &operator>>(std::istream &istrm, ArrayT<T> &array) {
     return array.ReadFrom(istrm);
 }
-
 #endif
--- mukhamedov_m_i/prj.test/arrayt_test.cpp
+++ mukhamedov_m_i/prj.test/arrayt_test.cpp
@@ -1,18 +1,221 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "arrayt/arrayt.hpp"
 #include "doctest/doctest.h"
-TEST_CASE("testing the T functions") {
-    CHECK_NOTHROW(ArrayT<int>{1, 2});
-    CHECK_NOTHROW(ArrayT<int>{1, 2}.ssize());
-    CHECK_NOTHROW(ArrayT<int>{1, 2});
-    ArrayT<int> a{1,2};
-    ArrayT<int> b;
-    a.remove(1);
-    a.insert(1,3);
-    b = a;
-    b[1] = 0;
-    std::cout << a << b;
+
+
+TEST_CASE("Test construct and copy") {
+    SUBCASE("Test with normal size") {
+        CHECK_NOTHROW(ArrayT<int>(3));
+    }
+    SUBCASE("Test with size = 0") {
+        CHECK_NOTHROW(ArrayT<int>(0));
+    }
+    SUBCASE("Test with negative size") {
+        CHECK_THROWS(ArrayT<int>(-1));
+    }
+    SUBCASE("Test copy") {
+        ArrayT<int> x(1);
+        x[0] = 1;
+        ArrayT<int> y = x;
+        y[0] = 2;
+        CHECK((x[0] != y[0]));
+    }
+    SUBCASE("Test copy yourself") {
+        ArrayT<int> x(2);
+        x[0] = 404;
+        x[1] = 404;
+        x = x;
+        CHECK((x[0] == 404 && x[1] == 404));
+    }
 }
+TEST_CASE("Test with operator[]") {
+    SUBCASE("Test with middle index") {
+        ArrayT<int> x(3);
+        CHECK_NOTHROW(x[1] = 404);
+        CHECK((x[1] == 404));
+    }
+    SUBCASE("Test with begin index") {
+        ArrayT<int> x(3);
+        CHECK_NOTHROW(x[0] = 404);
+        CHECK((x[0] == 404));
+    }
+    SUBCASE("Test with last index") {
+        ArrayT<int> x(3);
+        CHECK_NOTHROW(x[2] = 404);
+        CHECK((x[2] == 404));
+    }
+    SUBCASE("Test with out of range index > size") {
+        ArrayT<int> x(3);
+        CHECK_THROWS(x[4]);
+    }
+    SUBCASE("Test with out of range index == size") {
+        ArrayT<int> x(3);
+        CHECK_THROWS(x[3]);
+    }
+    SUBCASE("Test with out of range index < 0") {
+        ArrayT<int> x(3);
+        CHECK_THROWS(x[-1]);
+    }
+}
+
+TEST_CASE("Test of ssize, resize, insert, remove function") {
+    SUBCASE("size == size") {
+        CHECK((ArrayT<int>(5).ssize() == 5));
+    }
+    SUBCASE("Resize to size+1") {
+        ArrayT<int> x(1);
+        x.resize(x.ssize() + 1);
+        CHECK_NOTHROW(x[1]);
+    }
+    SUBCASE("Resize to 0") {
+        ArrayT<int> x(1);
+        CHECK_THROWS(x.resize(0));
+    }
+    SUBCASE("Resize to size < 0") {
+        ArrayT<int> x(1);
+        CHECK_THROWS(x.resize(-1));
+    }
+    ArrayT<int> test(3);
+    ArrayT<int> test_i(0);
+    int size = test.ssize();
+    for (int i = 0; i < size; i++) {
+        test[i] = i + 1;
+    }
+    SUBCASE("insert to middle") {
+        test_i = test;
+        test_i.insert(1,4);
+        CHECK((test_i[0] == test[0]));
+        CHECK((test_i[1] == 4));
+        CHECK((test_i[2] == test[1]));
+        CHECK((test_i[3] == test[2]));
+        CHECK((test_i.ssize() == size + 1));
+    }
+    SUBCASE("insert to begin") {
+        test_i = test;
+        test_i.insert(0,4);
+        CHECK((test_i[0] == 4));
+        CHECK((test_i[1] == test[0]));
+        CHECK((test_i[2] == test[1]));
+        CHECK((test_i[3] == test[2]));
+        CHECK((test_i.ssize() == size + 1));
+    }
+    SUBCASE("insert to end") {
+        test_i = test;
+        test_i.insert(3,4);
+        CHECK((test_i[0] == test[0]));
+        CHECK((test_i[1] == test[1]));
+        CHECK((test_i[2] == test[2]));
+        CHECK((test_i[3] == 4));
+        CHECK((test_i.ssize() == size + 1));
+    }
+    SUBCASE("insert to invalid position < 0") {
+        test_i = test;
+        CHECK_THROWS(test_i.insert(-1,4));
+    }
+    SUBCASE("insert to invalid position > size") {
+        test_i = test;
+        CHECK_THROWS(test_i.insert(4,4));
+    }
+
+    SUBCASE("remove from middle") {
+        test_i = test;
+        test_i.remove(1);
+        CHECK((test_i[0] == test[0]));
+        CHECK((test_i[1] == test[2]));
+        CHECK((test_i.ssize() == size - 1));
+    }
+    SUBCASE("remove from begin") {
+        test_i = test;
+        test_i.remove(0);
+        CHECK((test_i[0] == test[1]));
+        CHECK((test_i[1] == test[2]));
+        CHECK((test_i.ssize() == size - 1));
+    }
+    SUBCASE("remove from end") {
+        test_i = test;
+        test_i.remove(2);
+        CHECK((test_i[0] == test[0]));
+        CHECK((test_i[1] == test[1]));
+        CHECK((test_i.ssize() == size - 1));
+    }
+    SUBCASE("remove from invalid position < 0") {
+        test_i = test;
+        CHECK_THROWS(test_i.remove(-1));
+    }
+    SUBCASE("remove from invalid position == size") {
+        test_i = test;
+        CHECK_THROWS(test_i.remove(3));
+    }
+    SUBCASE("remove from invalid position > size") {
+        test_i = test;
+        CHECK_THROWS(test_i.remove(4));
+    }
+}
+TEST_CASE("My tests") {
+    ArrayT<int> a(4);
+
+    SUBCASE("Test: inserting value") {
+        for (int i = 0; i < 4; i++) {
+            a[i] = double(i+1);
+        }
+        a.insert(4, 5);
+
+        for (int i = 0; i < 5; i++) {
+            CHECK(a[i] == double(i+1));
+        }
+        CHECK(a.ssize() == 5);
+
+        a.insert(5, 5);
+        CHECK(a.ssize() == 6);
+    }
+
+    SUBCASE("Test: removing values"){
+        ArrayT<int> b(4);
+        for (int i = 0; i < 4; i++) {
+            b[i] = double(i+1);
+        }
+        b.insert(4, 5);
+
+        b.insert(5, 5);
+        CHECK(b.ssize() == 6);
+
+        b.remove(2);
+        CHECK(b[0] == 1);
+        CHECK(b[1] == 2);
+        CHECK(b[2] == 4);
+        CHECK(b[3] == 5);
+        CHECK(b.ssize() == 5);
+    }
+}
+//A ar;
+//new size == 0
+//index - 0 +
+//resize
+//size == new ssize
+//new elem ==
+//index - 0+
+//a[index] = elem and then a[index] == elem
+//and this with every
+//empty to array
+
+
+
+
+//#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+//#include "arrayt/arrayt.hpp"
+//#include "doctest/doctest.h"
+//TEST_CASE("testing the T functions") {
+//    CHECK_NOTHROW(ArrayT<int> a(3));
+//    CHECK_NOTHROW(ArrayT<int>{1, 2}.ssize());
+//    CHECK_NOTHROW(ArrayT<int>{1, 2});
+//    ArrayT<int> a{1,2};
+//    ArrayT<int> b;
+//    a.remove(1);
+//    a.insert(1,3);
+//    b = a;
+//    b[1] = 0;
+//    std::cout << a << b;
+//}
 //A ar;
 //new size == 0
 //index - 0 +
